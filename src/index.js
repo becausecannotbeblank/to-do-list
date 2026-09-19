c12ec7010f054f4e19d5100a28285873d1dbaffe
becausecannotbeblank <resubuhtig@proton.me>
2026-09-19 15:44:17 +0200

    Dark mode added.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+
+function setTheme(){
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
+
+document.getElementById("switchTheme").addEventListener("click", setTheme);
